f2944334c695b3ea999fc38b4860dd24acf79cb2
goobdevTM <getfreegame631@gmail.com>
2026-04-02 19:22:49 -0600

    tgftntrxdsh

--- scripts/button_animations.gd
+++ scripts/button_animations.gd
@@ -1,18 +1,29 @@
 extends Control
 
+@export var offset : Vector2 = Vector2(0.5,0.5)
+
 func _ready() -> void:
 	for control : Control in get_children():
 		if control is Button:
+			var button_size : Vector2 = Vector2(0,0)
+			if get_node(self.get_path()) is BoxContainer:
+				button_size = Vector2(size.x, control.custom_minimum_size.y)
+			else:
+				button_size = Vector2(control.size.x, control.size.y)
+			control.pivot_offset = Vector2(button_size.x * offset.x, button_size.y * offset.y)
 			control.connect("mouse_entered", button_hover.bind(control))
 			control.connect("mouse_exited", button_normal.bind(control))
 			control.connect("button_down", button_pressed.bind(control))
 			control.connect("button_up", button_hover.bind(control))
 			
 func button_hover(button : Button) -> void:
-	
+	var tween : Tween = create_tween()
+	tween.tween_property(button, "scale", Vector2(1.05,1.05), 0.1)
 	
 func button_normal(button : Button) -> void:
-	pass
+	var tween : Tween = create_tween()
+	tween.tween_property(button, "scale", Vector2(1,1), 0.1)
 	
 func button_pressed(button : Button) -> void:
-	pass
+	var tween : Tween = create_tween()
+	tween.tween_property(button, "scale", Vector2(0.98,0.98), 0.1)
